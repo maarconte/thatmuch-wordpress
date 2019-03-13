7b3e1b71db4582c95ac2c5921b00cf3423237576
Mathilde Arconte <signup@marconte.fr>
2019-03-13 11:14:11 +0100

    Section Contact + Fond étoilé

--- templates/section-contact.php
+++ templates/section-contact.php
@@ -11,98 +11,17 @@
  */
  ?>
 
-  <section class="section-contact
-  <? if(get_sub_field('fond') == "Couleur"):?> bg-primary
-  <? elseif(get_sub_field('fond') == "Gris"):?> bg-light<? endif;?>">
-    <!-- Section background: image -->
-      <? if(get_sub_field('fond') == "Image"):?>
-      <div class="section-background-image"  style="
-            <? if(get_sub_field('image')):?>
-            background-image:url(<? echo the_sub_field('image') ?>);
-            <? endif;?>"></div>
-      <? endif;?>
-    <!-- Section background: image -->
+  <section class="section-contact">
     <div class="container">
+<div class="row">
+
+<div class="contact-form  col-sm-6">
       <!-- Title -->
       <?php if(get_sub_field('title') ) : ?>
   <h2 class="section-title"><?php echo get_sub_field('title'); ?></h2>
 <?php endif; ?>
 <!-- Title -->
-<div class="row">
-<? if (get_sub_field('contact_infos') == "Oui"): ?>
-  <div class="col-sm-6">
-    <ul class="contact-infos">
-      <li class="contact-infos-phone">
-        <i class="fas fa-mobile fa-2x mr-2"></i>
-        <?php if (get_field('phone', 'option') ) : ?>
-          <span><?php echo get_field('phone','option'); ?></span>
-        <?php endif; ?>
-      </li>
-      <li class=" contact-infos-address">
-        <i class="fas fa-map-marker-alt fa-2x mr-2"></i>
-          <?php if (get_field('adress', 'option') ) : ?>
-            <span><?php echo get_field('adress','option'); ?></span>
-          <?php endif; ?>
-      </li>
-      <li class="contact-infos-hours">
-        <i class="far fa-clock fa-2x mr-2"></i>
-        <?php if (get_field('hours', 'option') ) : ?>
-          <span><?php echo get_field('hours','option'); ?></span>
-        <?php endif; ?>
-      </li>
-    </ul>
-    <?php if (have_rows('rs', 'options')) : ?>
-      <ul class="contact-infos-rs">
-        <?php while ( have_rows('rs', 'options') ) : the_row(); ?>
-          <?php if (get_sub_field('facebook') ) : ?>
-              <li>
-                <a href="<?php the_sub_field('facebook');?>">
-                  <i class="fab fa-facebook" aria-hidden="true"></i>
-                </a>
-              </li>
-            <?php endif; ?>
-          <?php if (get_sub_field('twitter') ) : ?>
-              <li>
-                <a href="<?php the_sub_field('twitter');?>">
-                  <i class="fab fa-twitter" aria-hidden="true"></i>
-                </a>
-              </li>
-            <?php endif; ?>
-          <?php if (get_sub_field('instagram') ) : ?>
-              <li>
-                <a href="<?php the_sub_field('instagram');?>">
-                  <i class="fab fa-instagram" aria-hidden="true"></i>
-                </a>
-              </li>
-            <?php endif; ?>
-          <?php if (get_sub_field('google') ) : ?>
-              <li>
-                <a href="<?php the_sub_field('google');?>">
-                  <i class="fab fa-google" aria-hidden="true"></i>
-                </a>
-              </li>
-            <?php endif; ?>
-          <?php if (get_sub_field('linkedin') ) : ?>
-              <li>
-                <a href="<?php the_sub_field('linkedin');?>">
-                  <i class="fab fa-linkedin" aria-hidden="true"></i>
-                </a>
-              </li>
-            <?php endif; ?>
-          <?php if (get_sub_field('youtube') ) : ?>
-              <li>
-                <a href="<?php the_sub_field('youtube');?>">
-                  <i class="fab fa-youtube" aria-hidden="true"></i>
-                </a>
-              </li>
-            <?php endif; ?>
-        <? endwhile;?>
-      </ul>
-    <? endif;?>
-  </div>
-<? endif;?>
-  <div class="contact-form <? if (get_sub_field('contact_infos') == "Oui"): ?> col-sm-6 <? else : ?> col-sm-12 full-width<?endif; ?>">
-    <!-- Contact form -->
+<!-- Contact form -->
     <?php $form = get_sub_field('contact_form');?>
     <?php if($form) : ?>
 
@@ -111,6 +30,27 @@
     <?php endif; ?>
     <!-- Contact form -->
   </div>
+  <div class="col-sm-6 devis-gratuit">
+<?php if ( get_sub_field('text') ) : ?>
+  <p><?php echo get_sub_field('text'); ?></p>
+<?php endif; ?>
+<!-- Button -->
+<?php if (have_rows('button')) : ?>
+            <?php while ( have_rows('button') ) : the_row(); ?>
+                <?php if (get_sub_field('link') == 'Externe' && get_sub_field('label') && get_sub_field('url') ) : ?>
+                    <a href="<?php the_sub_field('url'); ?>" class="btn btn-primary d-block m-auto"><?php the_sub_field('label'); ?></a>
+                <?php endif; ?>
+                <?php if (get_sub_field('link') == 'Interne' && get_sub_field('label') && get_sub_field('int_url') ) : ?>
+                    <a href="<?php the_sub_field('int_url'); ?>" class="btn btn-primary d-block m-auto">
+                        <?php the_sub_field('label'); ?>
+                    </a>
+                <?php endif; ?>
+            <?php endwhile; ?>
+        <?php endif; ?>
+        <!-- Button -->
+
+  </div>
+
 </div>
     </div>
  </section>

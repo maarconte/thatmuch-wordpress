51506b44cff9bb501aa991e98eff3083378a6da9
Mathilde Arconte <signup@marconte.fr>
2019-04-17 23:09:51 +0200

    screenshot + wysiwyg service

--- acf.php
+++ acf.php
@@ -1207,7 +1207,7 @@ acf_add_local_field_group(array(
 									'key' => 'field_5ba4ae6586491',
 									'label' => 'Texte',
 									'name' => 'text',
-									'type' => 'textarea',
+									'type' => 'wysiwyg',
 									'required' => 0,
 									'conditional_logic' => 0,
 									'wrapper' => array(

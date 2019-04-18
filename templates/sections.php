61e1936825378a67182cfa7ba272c7869b22c0f0
florian <florian.breton01@gmail.com>
2019-04-18 20:00:29 +0200

    css

--- templates/sections.php
+++ templates/sections.php
@@ -12,7 +12,7 @@
 <? /* Template Name: Sections */ ?>
 
 <? get_header(); ?>
-<div class="parallax">
+<div class="parallax parallax-fond">
     <div data-offset="35" class="decors decors-layer5" style="background-image: url(<?php echo site_url(); ?>/wp-content/themes/thatmuch-wordpress/assets/images/fond-points-2.png);"></div>
     <div data-offset="21" class="decors decors-layer4" style="background-image: url(<?php echo site_url(); ?>/wp-content/themes/thatmuch-wordpress/assets/images/fond-etoile-1.png);"></div>
 </div>

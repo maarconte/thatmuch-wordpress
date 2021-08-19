9cdddc565c5b07c1bff8c4e45aba4cf30edd195a
Mathilde Arconte <signup@marconte.fr>
2021-08-19 10:31:55 +0200

    text color blog

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,35 @@
 /*
  * GULP CONFIG
  *
- * Desciption:  Clean gulpfile for web development workflow containing
+ * Desciption: Clean gulpfile for web development workflow containing
  *              - compiling/optimization of sass, javascript and images from assets to dist and vendors
  *              - browsersync
  *              - cache-busting
  *              - modernizr
  *              - vendor handling through glulp-vendors.json
  *
- * Usage:       - `gulp` (to run the whole process)
+ * Usage: - `gulp` (to run the whole process)
  *              - `gulp watch` (to watch for changes and compile if anything is being modified)
- *              - `modernizr -c assets/scripts/modernizr-config.json -d assets/scripts` to generate the modernizr.js file from the config-file
  *              - add vendor-requirements to gulp-vendors.json, they will be compiled/bundled by `gulp` (restart `gulp watch`)
  *
- * Author:      ThatMuch (https://flurinduerst.ch)
+ * Author: thatmuch
  *
- * Version:     2.3.1
+ * Version: 2.3.1
  *
 */
 
 
 /* SETTINGS
-/===================================================== */
+  /=  ================================================== =  = */
 // local domain used by browsersync
-var browsersync_proxy = "thatmuch.local";
+var browsersync_proxy = "localhost:10048";
 
 // default asset paths
 var assets = {
   css: ['assets/styles/style.scss'],
   css_watch: ['assets/styles/**/*.scss'],
+  admin_css: ['assets/AdminStyles/style.scss'],
+  admin_css_watch: ['assets/AdminStyles/**/*.scss'],
   javascript: ['assets/scripts/*.js'],
   images: ['assets/images/*.*'],
   fonts: ['assets/fonts/*.*']
@@ -58,7 +59,7 @@ var vendors = require('./gulp-vendors.json');
 
 
 /* DEPENDENCIES
-/===================================================== */
+  /=  ================================================== =  = */
 // general
 var gulp = require('gulp');
 var concat = require('gulp-concat');
@@ -69,15 +70,12 @@ var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
 var autoprefixer = require('gulp-autoprefixer');
-var csscomb       = require('gulp-csscomb');
-// traduction
-var wpPot         = require('gulp-wp-pot');
 // cache busting
 var rev = require('gulp-rev');
 // js
-var uglify = require('gulp-uglify');
-// images
-// var imagemin = require('gulp-imagemin');
+var uglify = require('gulp-uglify-es').default;
+// traduction
+var wpPot = require('gulp-wp-pot');
 // error handling with notify & plumber
 var notify = require("gulp-notify");
 var plumber = require('gulp-plumber');
@@ -87,32 +85,32 @@ var watch = require('gulp-watch');
 var del = require('del');
 // zip
 var zip = require('gulp-zip');
-var runSequence   = require('run-sequence');
-
 
 /* TASKS
-/===================================================== */
+  /=  ================================================== =  = */
 
 /* CLEAN
 /––––––––––––––––––––––––*/
 // delete compiled files/folders (before running the build)
 // css
-gulp.task('clean:css', function() { return del(['dist/*.css', 'dist/rev-manifest.json'])});
-gulp.task('clean:cachebust', function() { return del(['./style-*.min.css'])});
-gulp.task('clean:javascript', function() { return del(['dist/*.js'])});
+gulp.task('clean:css',function () { return del(['dist/*.css','dist/rev-manifest.json']) });
+gulp.task('clean:admin_css',function () { return del(['dist/*.css']) });
+gulp.task('clean:cachebust',function () { return del(['./style-*.min.css']) });
+gulp.task('clean:javascript',function () { return del(['dist/*.js']) });
 
 /* BROWSERSYNC
 /––––––––––––––––––––––––*/
 // initialize Browser Sync
-gulp.task('browsersync', function() {
+gulp.task('browsersync',function () {
   browserSync.init({
     proxy: browsersync_proxy,
-    notify: true,
+    notify: false,
     open: false,
     snippetOptions: {
       whitelist: ['/wp-admin/admin-ajax.php'],
       blacklist: ['/wp-admin/**']
-    }
+    },
+    // reloadDelay: 800
   });
 });
 
@@ -122,18 +120,35 @@ gulp.task('browsersync', function() {
 // from:    assets/styles/main.css
 // actions: compile, minify, prefix, rename
 // to:      ./style.min.css
-gulp.task('css', ['clean:css'], function() {
-  return gulp.src(assets['css'].concat(vendors['css']))
-    .pipe(plumber({errorHandler: notify.onError("Error")}))
+gulp.task('css',gulp.series('clean:css',function () {
+  return gulp
+    .src(assets['css'].concat(vendors['css']))
+    .pipe(plumber({ errorHandler: notify.onError("<%= error.message %>") }))
     .pipe(concat('style.min.css'))
     .pipe(sass())
-    .pipe(autoprefixer('last 2 version', { cascade: false }))
-    .pipe(csscomb())
+    .pipe(autoprefixer('last 2 version',{ cascade: false }))
     .pipe(cleanCSS())
     .pipe(rename('./style.min.css'))
     .pipe(gulp.dest('./'))
     .pipe(browserSync.stream());
-});
+}));
+/* ADMIN CSS
+/––––––––––––––––––––––––*/
+// from:    assets/styles/main.css
+// actions: compile, minify, prefix, rename
+// to:      ./style.min.css
+gulp.task('admin_css',gulp.series('clean:admin_css',function () {
+  return gulp
+    .src(assets['admin_css'])
+    .pipe(plumber({ errorHandler: notify.onError("<%= error.message %>") }))
+    .pipe(concat('admin_style.min.css'))
+    .pipe(sass())
+    .pipe(autoprefixer('last 2 version',{ cascade: false }))
+    .pipe(cleanCSS())
+    .pipe(rename('./admin_style.min.css'))
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.stream());
+}));
 
 
 /* CSS CACHE BUSTING
@@ -141,13 +156,15 @@ gulp.task('css', ['clean:css'], function() {
 // from:    dist/style.min.css
 // actions: create busted version of file
 // to:      dist/style-[hash].min.css
-gulp.task('cachebust', ['clean:cachebust', 'css'], function() {
-  return gulp.src('./style.min.css')
+gulp.task('cachebust',gulp.series('clean:cachebust','css',function () {
+  return gulp
+    .src('./style.min.css')
     .pipe(rev())
     .pipe(gulp.dest('./'))
-    .pipe(rev.manifest({merge: true}))
+    .pipe(rev.manifest({ merge: true }))
     .pipe(gulp.dest('./'))
-});
+    .pipe(browserSync.reload({ stream: true }));
+}));
 
 
 /* JAVASCRIPT
@@ -156,36 +173,38 @@ gulp.task('cachebust', ['clean:cachebust', 'css'], function() {
 // actions: concatinate, minify, rename
 // to:      ./script.min.css
 // note:    modernizr.js is concatinated first in .pipe(order)
-gulp.task('javascript', ['clean:javascript'], function() {
-  return gulp.src(assets['javascript'].concat(vendors['javascript']))
+gulp.task('javascript',gulp.series('clean:javascript',function () {
+  return gulp
+    .src(assets['javascript'].concat(vendors['javascript']))
     .pipe(order([
       'assets/scripts/modernizr.js',
       'assets/scripts/*.js'
-    ], { base: './' }))
-    .pipe(plumber({errorHandler: notify.onError("Error")}))
+    ],{ base: './' }))
+    .pipe(plumber({ errorHandler: notify.onError("<%= error.message %>") }))
     .pipe(concat('script.min.js'))
     .pipe(uglify())
     .pipe(rename('./script.min.js'))
     .pipe(gulp.dest('./'))
     .pipe(browserSync.stream());
-});
+}));
 
 /* LANGUAGES
 /––––––––––––––––––––––––*/
 // from:    ./
 // actions: Generates pot files for WordPress plugins and themes.
 // to:      ./languages
-gulp.task('makepot', function () {
-  return gulp.src(['**/*.php'])
+gulp.task('makepot',function () {
+  return gulp
+    .src(['**/*.php'])
     .pipe(wpPot({
       domain: 'thatmuch',
       destFile: 'thatmuch.pot',
       package: 'thatmuch',
       bugReport: 'https://example.com/bugreport/',
-      team: 'ThatMuch <>'
+      team: 'thatmuch <>'
     }))
     .pipe(gulp.dest('languages/thatmuch.pot'))
-    .pipe(browserSync.reload({stream:true}));
+    .pipe(browserSync.reload({ stream: true }));
 });
 
 
@@ -193,38 +212,41 @@ gulp.task('makepot', function () {
 /––––––––––––––––––––––––*/
 // watch for modifications in
 // styles, scripts, images, php files, html files
-gulp.task('watch',  ['browsersync'], function() {
-  gulp.watch(assets['css_watch'], ['css', 'cachebust']);
-  gulp.watch(assets['javascript'], ['javascript']);
-  gulp.watch('**/*.php', browserSync.reload);
-  gulp.watch('*.html', browserSync.reload);
+gulp.task('watch',gulp.parallel('browsersync',function () {
+  watch(assets['css_watch'],gulp.series('css','cachebust'));
+  watch(assets['admin_css_watch'],gulp.series('admin_css'));
+  watch(assets['javascript'],gulp.series('javascript'));
+  watch('**/*.php',browserSync.reload);
+  watch('*.html',browserSync.reload);
+}));
+
+gulp.task('build-clean',function () {
+  return del(['dist/**/*']);
 });
 
-gulp.task('build-clean', function() {
-  del(['dist/**/*']);
-});
-
-gulp.task('build-copy', function() {
-  return gulp.src(build_files)
+gulp.task('build-copy',function () {
+  return gulp
+    .src(build_files)
     .pipe(gulp.dest('dist/thatmuch'));
 });
 
-gulp.task('build-zip', function() {
-  return gulp.src('dist/**/*')
+gulp.task('build-zip',function () {
+  return gulp
+    .src('dist/**/*')
     .pipe(zip('thatmuch.zip'))
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build-delete', function() {
-  del(['dist/**/*', '!dist/thatmuch.zip']);
-});
-
-gulp.task('build', function(callback) {
-  runSequence('default','build-clean', 'build-copy', 'build-zip', 'build-delete');
+gulp.task('build-delete',function () {
+  return del(['dist/**/*','!dist/thatmuch.zip']);
 });
 
 
 /* DEFAULT
 /––––––––––––––––––––––––*/
 // default gulp tasks executed with `gulp`
-gulp.task('default', ['css', 'cachebust', 'javascript','makepot']);
+gulp.task('default',gulp.series('css','cachebust'));
+
+gulp.task('build',
+  gulp.series('default','build-clean','build-copy','build-zip','build-delete')
+);

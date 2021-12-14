e537d46925072facb4e2f7448f41b90780730c56
Mathilde Arconte <signup@marconte.fr>
2021-12-14 13:49:35 +0100

    landing page

--- footer.php
+++ footer.php
@@ -8,60 +8,7 @@
 
 	</div><!-- #content -->
     <footer class="footer">
-      <div class="container">
-        <div class="row inner">
-        <?php if (have_rows('rs', 'options')) : ?>
-        <div class="col-sm-3">
-      <ul class="footer-rs">
-        <?php while ( have_rows('rs', 'options') ) : the_row(); ?>
-          <?php if (get_sub_field('facebook') ) : ?>
-              <li>
-                <a href="<?php the_sub_field('facebook');?>">
-                  <i class="fab fa-facebook" aria-hidden="true"></i>
-                </a>
-              </li>
-            <?php endif; ?>
-          <?php if (get_sub_field('twitter') ) : ?>
-              <li>
-                <a href="<?php the_sub_field('twitter');?>">
-                  <i class="fab fa-twitter" aria-hidden="true"></i>
-                </a>
-              </li>
-            <?php endif; ?>
-          <?php if (get_sub_field('instagram') ) : ?>
-              <li>
-                <a href="<?php the_sub_field('instagram');?>">
-                  <i class="fab fa-instagram" aria-hidden="true"></i>
-                </a>
-              </li>
-            <?php endif; ?>
-          <?php if (get_sub_field('google') ) : ?>
-              <li>
-                <a href="<?php the_sub_field('google');?>">
-                  <i class="fab fa-google" aria-hidden="true"></i>
-                </a>
-              </li>
-            <?php endif; ?>
-          <?php if (get_sub_field('linkedin') ) : ?>
-              <li>
-                <a href="<?php the_sub_field('linkedin');?>">
-                  <i class="fab fa-linkedin" aria-hidden="true"></i>
-                </a>
-              </li>
-            <?php endif; ?>
-          <?php if (get_sub_field('youtube') ) : ?>
-              <li>
-                <a href="<?php the_sub_field('youtube');?>">
-                  <i class="fab fa-youtube" aria-hidden="true"></i>
-                </a>
-              </li>
-            <?php endif; ?>
-        <? endwhile;?>
-      </ul>
-        </div>
-    <? endif;?>
-        </div>
-      </div>
+
     <div class="credits">
       <div class="container">
       <div class="inner">
---
+++ front-page.php
@@ -0,0 +1,67 @@
+<?
+/**
+ * The template displaying the posts-overview
+ *
+ * @author      ThatMuch
+ * @version     0.1.0
+ * @since       undefined 0.1.0
+ */
+
+?>
+
+<? get_header(); ?>
+
+  <main class="content-area">
+<section class="hero">
+	<div class="container">
+		<img src="<?php echo get_template_directory_uri() ?>/assets/images/Logo.png" alt="thatmuch logo">
+		<h1>We'll be back !</h1>
+		<p>THATMUCH est en pleine refonte, le site sera bientôt de retour ! N'hesitez pas à nous contacter pour toute demande.</p>
+	</div>
+</section>
+<section class="contact">
+	<div class="container">
+		<script charset="utf-8" type="text/javascript" src="//js-eu1.hsforms.net/forms/shell.js"></script>
+	<script>
+	  hbspt.forms.create({
+		region: "eu1",
+		portalId: "25329660",
+		formId: "bd11f958-5279-47cf-8734-74e7124351ab"
+	});
+	</script>
+	</div>
+</section>
+<section class="content">
+	<div class="container">
+		<div class="row">
+						<div class="col-sm-12">
+				<img src="<?php echo get_template_directory_uri() ?>/assets/images/Illustration.png" alt="thatmuch - illustration alien">
+			</div>
+			<div class="col-sm-4">
+				<div class="block">
+					<h2>Mission</h2>
+					<p><strong>THATMUCH</strong> vous accompagne dans toutes les étapes de votre projet afin d’atteindre vos objectifs et le faire décoller. Notre expertise et expérience dans la <strong>création et le suivi des projets</strong> , nous permettent de mettre en œuvre une réponse efficace à vos demandes.</p>
+				</div>
+</div>
+
+<div class="col-sm-4">
+				<div class="block">
+					<h2>Valeurs</h2>
+					<p>Nous attachons une importance particulière à <strong>l’humain, l’échange et la collaboration</strong>  avec le client. Notre principale priorité est de satisfaire le client en livrant rapidement et régulièrement des solutions qui apportent de la valeur.</p>
+				</div>
+</div>
+<div class="col-sm-4">
+				<div class="block">
+					<h2>Vision</h2>
+					<p>Nous utilisons les <strong>méthodes agiles</strong>  pour le pilotage et la réalisation de vos projets.</p>
+				</div>
+</div>
+	<div class="col-sm-12 d-flex justify-content-center">
+				<a href="https://www.canva.com/design/DAEyc40-uP0/view?utm_content=DAEyc40-uP0&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink" class="btn btn-primary" target="_blank">Notre portfolio</a>
+</div>
+		</div>
+	</div>
+</section>
+  </main>
+
+<? get_footer(); ?>
